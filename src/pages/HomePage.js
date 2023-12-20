92d447cc688f33f5064d99fc2e70b1c08c7af27a
Arif <62802641+Arif-hossaian@users.noreply.github.com>
2023-12-20 18:09:27 +0000

    Update 9 files

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Layout from '../layout/Layout'
-import Banner from '../components/Banner'
-import AmazeNft from '../components/AmazeNft'
-import TopCollectionOver from '../components/TopCollectionOver'
-import CollectionFrtNft from '../components/CollectionFrtNft'
-import SignUpNft from '../components/SignUpNft'
-import DiscoverNfts from '../components/DiscoverNfts'
+import React from "react";
+import Layout from "../layout/Layout";
+import Banner from "../components/Banner";
+import AmazeNft from "../components/AmazeNft";
+import TopCollectionOver from "../components/TopCollectionOver";
+import CollectionFrtNft from "../components/CollectionFrtNft";
+import SignUpNft from "../components/SignUpNft";
+import DiscoverNfts from "../components/DiscoverNfts";
 
 const Home = () => {
   return (
     <Layout>
-        <Banner />
-        <AmazeNft />
-        <TopCollectionOver />
-        <CollectionFrtNft />
-        <SignUpNft />
-        <DiscoverNfts />
+      <Banner />
+      <AmazeNft />
+      <TopCollectionOver />
+      <CollectionFrtNft />
+      <SignUpNft />
+      <DiscoverNfts />
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
